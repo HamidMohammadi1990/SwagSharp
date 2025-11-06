6ff5f6867328e2778ef0be94415b7de82a86ec45
HAMID-PC <40312758@PC-292>
2025-11-06 16:04:03 +0330

    create generate structure model

---
+++ SwagSharp.Api/DTOs/GenerateServiceRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace SwagSharp.Api.DTOs;
+
+public record GenerateServiceRequest
+{
+    public IFormFile File { get; init; }
+    public string ModelsNameSpace { get; init; }
+    public string InterfacesNameSpace { get; init; }
+    public string ServicesNameSpace { get; init; }
+
+
+    public bool IsValid()
+    {
+        return File is not null &&
+            File?.Length > 0 &&
+            !string.IsNullOrWhiteSpace(ModelsNameSpace) &&
+            !string.IsNullOrWhiteSpace(InterfacesNameSpace) &&
+            !string.IsNullOrWhiteSpace(ServicesNameSpace);
+    }
+}
